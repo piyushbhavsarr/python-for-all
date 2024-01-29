4dd0b207d41a443d509ff2e31f9e1fecad9fbb7f
piyushbhavsarr <iampiyush.bhavsar@gmail.com>
2024-01-29 17:46:18 +0530

    operations on list

--- data-structures/lists.py
+++ data-structures/lists.py
@@ -1,6 +1,6 @@
 #List and Operations on it.
 first_list = []
-n=5
+n=4
 for i in range (0,n):
     inp = input("Enter Data to enter in the list :")
     first_list.append(inp)
@@ -21,3 +21,30 @@ print(first_list)
 first_list.insert(10, "inserted value")
 print(first_list)
 
+#list.remove(x) , here value which has to be removed must be entered.
+second_list.remove('Game') # Game is removed from the second list
+print(second_list)
+
+#list.pop(i) Remove the item at the given position in the list *square brackets should not be used
+
+second_list.pop() #if nothing is there, last elements is deleted
+print(second_list)
+
+second_list.pop(3) #remove the item present at the index location 3
+print(second_list)
+
+#list.index(x[, start[, end]])
+
+my_list = ['apple', 'banana', 'orange', 'banana', 'grape']
+
+# Find the index of the first occurrence of 'banana'
+index = my_list.index('banana')
+print(f"Index of 'banana': {index}")
+
+# Find the index of 'banana' starting from index 2
+index_starting_from_2 = my_list.index('grape', 2)
+print(f"Index of 'banana' starting from index 2: {index_starting_from_2}")
+
+# Find the index of 'banana' between index 2 (inclusive) and 4 (exclusive)
+index_within_range = my_list.index('banana', 0, 4)
+print(f"Index of 'banana' within the range [2, 4): {index_within_range}")
