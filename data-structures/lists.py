0c542148a822cea1b8b512c13893b389d823f16d
piyushbhavsarr <iampiyush.bhavsar@gmail.com>
2024-01-29 17:02:08 +0530

    more operations

--- data-structures/lists.py
+++ data-structures/lists.py
@@ -1,6 +1,6 @@
 #List and Operations on it.
 first_list = []
-n=10
+n=5
 for i in range (0,n):
     inp = input("Enter Data to enter in the list :")
     first_list.append(inp)
@@ -13,5 +13,11 @@ second_list = ['Game', 'Screen', 'Javascript', 'WebApps', 200, 400, 405]
 
 print(second_list)
 print("Merging both first + second")
-c = first_list.extend(second_list)
-print(c)
+first_list.extend(second_list)
+print(first_list)
+
+#list.insert(i, x) -> index, value
+
+first_list.insert(10, "inserted value")
+print(first_list)
+
