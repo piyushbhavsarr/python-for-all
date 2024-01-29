fe05876c063ed410dd327d8c6d2aa2f650129f48
piyushbhavsarr <iampiyush.bhavsar@gmail.com>
2024-01-29 14:15:34 +0530

    Initiation

---
+++ first_program.py
@@ -0,0 +1,10 @@
+#A very simple program, but not Hello World
+nameinput=input("What is your name: ")
+print("Hey",nameinput)
+print("Let's get started with the Python!")
+
+
+'''Output:
+What is your name: Piyush Bhavsar
+Hey Piyush Bhavsar
+Let's get started with the Python!'''
